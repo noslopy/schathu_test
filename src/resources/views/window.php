b1920a62b07d8375ae0be0b7afa47fa909f3fa20
noslopy <info@noslopy.dev>
2020-08-01 15:48:04 +0200

    ✨ setting up controller & view for video tool

--- src/resources/views/window.php
+++ src/resources/views/window.php
@@ -1,5 +1,8 @@
-<?php 
-    $all_content = file_get_contents("-");
+<?php
+    // window controller
+    // *****************
+    
+    $all_content = file_get_contents(env('STREAMERS_SOURCE', true));
     $content_json = json_decode($all_content, true);
     // var_dump(json_decode($content_json[0]['onlineparams'], true)['modeSpecific']['main']['hls']);
     foreach ($content_json as $member) {
@@ -8,20 +11,36 @@
             isset(json_decode($member['onlineparams'], true)['modeSpecific']['main']['hls'])
         )
         {
-            $src = array_values(json_decode($member['onlineparams'], true)['modeSpecific']['main']['hls'])[0];
+            $stream_src = array_values(json_decode($member['onlineparams'], true)['modeSpecific']['main']['hls'])[0];
         };
     };
 ?>
 
 <html>
     <head>
-        <script src="https://cdn.jsdelivr.net/npm/mediaelement@4.2.16/build/mediaelement-and-player.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
     </head>
     <body>
-    <video src="https:<?php echo $src;?>" width="320" height="240"
-		class="mejs__player"
-		data-mejsoptions='{"pluginPath": "/path/to/shims/", "alwaysShowControls": "true"}'></video>
-
+        <video id="video" style="height:100%;width:100%;"></video>
+        <script>
+            var video = document.getElementById('video');
+            var videoSrc = 'https:<?php echo $stream_src ?>';
+            if (Hls.isSupported()) {
+                var hls = new Hls();
+                hls.loadSource(videoSrc);
+                hls.attachMedia(video);
+                hls.on(Hls.Events.MANIFEST_PARSED, function() {
+                    video.play();
+                });
+            }
+            else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+                video.src = videoSrc;
+                video.addEventListener('loadedmetadata', function() {
+                    video.play();
+                });
+            }
+        </script>
     </body>
 </html>
 
